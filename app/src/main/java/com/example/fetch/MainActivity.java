20e1c599aec9f9dddd5e9e38cac75aa6167963ad
Belle <lianasevero3@gmail.comgit config --global user.email lianasevero3@gmail.com>
2019-04-06 13:47:44 -0400

    Test commit from android studio

--- app/src/main/java/com/example/fetch/MainActivity.java
+++ app/src/main/java/com/example/fetch/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Test commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
